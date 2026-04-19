50f166d367c33f40d9f9a1fbd76b139eda6f7405
Vitaliy <vitalianobro@gmail.com>
2026-04-19 09:49:23 +0300

    code revision
    add if __name__ == "__main__":
    add .env
    use json.load
    add media
    editing quotation marks
    add urlencode
    update README

--- render_website.py
+++ render_website.py
@@ -2,37 +2,40 @@ import json
 import os
 import math
 
+from environs import env
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
+from dotenv import load_dotenv
+
+
+PATH_TO_LIBRARY = env.str("PATH_TO_LIBRARY")
 
 
 def on_reload():
-    os.makedirs('pages', exist_ok=True)
+    os.makedirs("pages", exist_ok=True)
     env = Environment(
         loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
+        autoescape=select_autoescape(["html"])
     )
-
-    template = env.get_template('template.html')
-
-    with open("meta_data.json", "r", encoding="utf8") as my_file:
-        books_json = my_file.read()
-    books = json.loads(books_json)
+    template = env.get_template("template.html")
+    with open(PATH_TO_LIBRARY, "r", encoding="utf8") as my_file:
+        books = json.load(my_file)
     books = list(chunked(books, 2))
     pages = list(chunked(books, 10))
-    
     for i, page in enumerate(pages, 1):
         rendered_page = template.render(
-        books=page,
-        pages_count=math.ceil(len(pages)),
-        current_page=i
-        )
-        with open(f'pages/index{i}.html', 'w', encoding="utf8") as file:
+            books=page,
+            pages_count=math.ceil(len(pages)),
+            current_page=i
+            )
+        with open(f"pages/index{i}.html", "w", encoding="utf8") as file:
             file.write(rendered_page)
 
-on_reload()
 
-server = Server()
-server.watch('template.html', on_reload)
-server.serve(root='.')
+if __name__ == "__main__":
+    load_dotenv()
+    on_reload()
+    server = Server()
+    server.watch("template.html", on_reload)
+    server.serve(root=".")
